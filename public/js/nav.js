acac6d55c0a713225ba2e0d8b41039f87628275c
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2022-03-15 00:20:39 +0700

    long time no see

--- public/js/nav.js
+++ public/js/nav.js
@@ -9,7 +9,13 @@ const createNav = () => {
                     <input type="text" class="search-box" placeholder="search brand, product">
                     <button class="search-btn">search</button>
                 </div>
-                <a href="#"><img src="img/user.png" alt=""></a>
+                <a>
+                    <img src="img/user.png" id="user-img" alt="">
+                    <div class="login-logout-popup hide">
+                        <p class="account-info">Log in as, name</p>
+                        <button class="btn" id="user-btn">Log out</button>
+                    </div>
+                </a>
                 <a href="#"><img src="img/cart.png" alt=""></a>
             </div>
         </div>
@@ -23,3 +29,33 @@ const createNav = () => {
     `;
 }
 createNav();
+
+// nav popup
+const userImageButton = document.querySelector('#user-img');
+const userPop = document.querySelector('.login-logout-popup');
+const popupText = document.querySelector('.account-info');
+const actionBtn = document.querySelector('#user-btn');
+
+userImageButton.addEventListener('click', () => {
+    userPop.classList.toggle('hide');
+})
+
+window.onload = () => {
+    let user = JSON.parse(sessionStorage.user || null);
+    if (user != null) {
+        // means user is logged in
+        popupText.innerHTML = `log in as, ${user.name}`;
+        actionBtn.innerHTML = 'log out';
+        actionBtn.addEventListener('click', () => {
+            sessionStorage.clear();
+            location.reload();
+        })
+    } else {
+        // user is logged out
+        popupText.innerHTML = 'log in to place order';
+        actionBtn.innerHTML = 'log in';
+        actionBtn.addEventListener('click', () => {
+            location.href = '/login';
+        })
+    }
+}
